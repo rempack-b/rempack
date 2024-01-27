d51be2e90ed56418e9519fae61ef2909461dbe0c
rempack-b <rm@brantmartin.com>
2024-01-27 17:47:38 +0000

    Docs done. Toolchain done. It works?!

--- docker_build_script.sh
+++ docker_build_script.sh
@@ -1,4 +1,5 @@
-echo "running docker"
+#this is meant to be run inside the docker container
+#running this on your host machine will most likely fail
 cmake . -B cmake-build-docker --preset Release
 cmake --build cmake-build-docker
 chmod 777 -R cmake-build-docker
