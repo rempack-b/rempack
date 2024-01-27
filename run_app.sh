13e3dfb6ff7c30013fd8f65a7de86910e893d769
rempack-b <rm@brantmartin.com>
2024-01-27 17:47:40 +0000

    Release and Debug modes behave normally and there's a native preset to build for the tablet. Also some attempts to precomiple rmkit.h. It works, but not reliably.

--- run_app.sh
+++ run_app.sh
@@ -1,9 +1,11 @@
+#!/bin/bash
 #mostly stolen from rmkit
 #copies your app to the tablet, runs it, then waits for interrupt
 #before closing it and restarting the remarkable interface
 
 REMARKABLE_HOST="remarkable"
-APP=${1}
+APP_PATH=${1}
+APP=$(basename "${APP_PATH}")
 BASE_DIR="/home/root"
 RM_USER="root"
 
@@ -16,6 +18,7 @@ function cleanup() {
   ssh ${RM_USER}@${REMARKABLE_HOST} rm ${BASE_DIR}/${APP}
   ssh ${RM_USER}@${REMARKABLE_HOST} systemctl restart remux
   echo "FINISHED"
+  trap - EXIT
   exit 0
 }
 
@@ -23,7 +26,7 @@ trap cleanup EXIT
 trap cleanup SIGINT
 
 #this is probably brittle, I'm sure it's fine
-scp cmake-build-release-docker/${APP} ${RM_USER}@${REMARKABLE_HOST}:${BASE_DIR}/${APP}
+scp ${APP_PATH} ${RM_USER}@${REMARKABLE_HOST}:${BASE_DIR}/${APP}
 kill_remote_app
 echo "RUNNING ${APP}"
 #remove rm2fb-client if you're running on a RM1
