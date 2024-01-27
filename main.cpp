8474ee06ec98d460a67dfd4d411727f4b2eab8ed
rempack-b <rm@brantmartin.com>
2024-01-27 17:47:38 +0000

    Clean up a couple last things

--- main.cpp
+++ main.cpp
@@ -1,21 +1,15 @@
 #include <iostream>
-#include <stdio.h>
-#include <pthread.h>
 #include <rmkit.h>
 
 int main()
 {
     auto fb = framebuffer::get();
-    //auto dims = fb->get_display_size();
-    //auto width = std::get<0>(dims);
-    //auto height = std::get<1>(dims);
 
     fb->clear_screen();
 
     auto scene = ui::make_scene();
     ui::MainLoop::set_scene(scene);
     auto t = new ui::Text(0,0,200,50, "hellorld");
-    //v->pack_start(t);
     scene->add(t);
 
     while(true){
