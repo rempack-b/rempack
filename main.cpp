6b5e45349f59b24c60379caccd820110ae139062
rempack-b <rm@brantmartin.com>
2024-01-27 17:47:38 +0000

    Seems to mostly work

--- main.cpp
+++ main.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 //#include "rmkit/artifacts/rm/rmkit.h"
 
+//#define REMARKABLE
+#define FONT_EMBED_H "font_embed.h"
+#define RMKIT_IMPLEMENTATION
+
 #include "rmkit.h"
 
 int main()
 {
     auto fb = framebuffer::get();
-    auto dims = fb->get_display_size();
-    auto width = std::get<0>(dims);
-    auto height = std::get<1>(dims);
+    //auto dims = fb->get_display_size();
+    //auto width = std::get<0>(dims);
+    //auto height = std::get<1>(dims);
 
     fb->clear_screen();
 
