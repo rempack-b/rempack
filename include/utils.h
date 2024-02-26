b6da0a64e5d970695dbe92ba1825f1772e57270a
rempack-b <rm@brantmartin.com>
2024-02-26 23:25:12 +0000

    make text helpers less bad

--- include/utils.h
+++ include/utils.h
@@ -8,6 +8,7 @@
 #include <string>
 #include <algorithm>
 #include <vector>
+#include <iomanip>
 
 namespace utils{
     inline float sigmoid(float t, float a, float b){
@@ -31,5 +32,23 @@ namespace utils{
     inline void trim(std::string &s){
         ltrim(s);
         rtrim(s);
+    }
+
+    inline string stringifySize(uint64_t bytes)
+    {
+        const string suffix[] = {"B", "KB", "MB", "GB", "TB"};
+        char length = sizeof(suffix) / sizeof(suffix[0]);
+
+        int i = 0;
+        double dblBytes = bytes;
+
+        if (bytes > 1024) {
+            for (i = 0; (bytes / 1024) > 0 && i<length-1; i++, bytes /= 1024)
+                dblBytes = bytes / 1024.0;
+        }
+
+        stringstream ss;
+        ss << std::fixed << std::setprecision(2) << dblBytes << suffix[i];
+        return ss.str();
     }
 }
--- ui/text_helpers.h
+++ ui/text_helpers.h
@@ -12,9 +12,16 @@ namespace utils{
         return make_tuple(v.w, v.h);
     }
 
+    inline int line_height(ui::Style style = ui::Style::DEFAULT){
+        return stbtext::get_line_height(style.font_size);
+    }
+
+    //inline std::string wrap_string(std::string s, short fontSize, int &outHeight, int widthLimit, int heightLimit = -1){   }
+
     inline std::string clip_string(std::string s, int w, int h, short fontSize){
         std::stringstream ss;
         auto line_height = stbtext::get_line_height(fontSize);
+        auto sw = stbtext::get_text_size(" ", fontSize).w;
         auto lines = split_lines(s);
         int x = 0;
         int y = 0;
@@ -23,9 +30,8 @@ namespace utils{
             auto tokens = split(line, ' ');
             for (std::string &tk: tokens) {
                 auto token = tk;
-                //TODO: do this less bad
-                token.append(" ");
                 auto v = stbtext::get_text_size(token, fontSize);
+                v.w += sw;
                 if(x + v.w < w)
                     x += v.w;
                 else{
@@ -34,7 +40,7 @@ namespace utils{
                     y += line_height;
                     x = v.w;
                 }
-                ss << token;
+                ss << token << ' ';
             }
             ss << std::endl;
         }
