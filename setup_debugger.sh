ed7a3d1a107fd0cb92c7b1403e798c5230ee197e
rempack-b <rm@brantmartin.com>
2024-07-25 02:48:39 +0000

    Fix up a few things about the remote runner

--- setup_debugger.sh
+++ setup_debugger.sh
@@ -8,7 +8,7 @@ RM_USER="root"
 
 ssh ${RM_USER}@${REMARKABLE_HOST} "systemctl stop remux; systemctl stop xochitl; killall gdbserver; killall ${APP}"
 #this is probably brittle, I'm sure it's fine
-scp ${APP_PATH} ${RM_USER}@${REMARKABLE_HOST}:${BASE_DIR}/${APP}
+#scp ${APP_PATH} ${RM_USER}@${REMARKABLE_HOST}:${BASE_DIR}/${APP}
 echo "RUNNING ${APP}"
 #remove rm2fb-client if you're running on a RM1
 ssh ${RM_USER}@${REMARKABLE_HOST} "cd ${BASE_DIR}; gdbserver :1243 /opt/bin/rm2fb-client ./${APP}" &
